9c759b631f7a194efb86c06e1935cdc2856200d3
Micah Mangione <micah@connectio.us>
2021-01-10 03:48:03 -0700

    Add new elements to the NetSuiteConnection class.

--- netsuitesdk/connection.py
+++ netsuitesdk/connection.py
@@ -1,21 +1,23 @@
 from .api.accounts import Accounts
 from .api.classifications import Classifications
-from .api.departments import Departments
 from .api.currencies import Currencies
-from .api.locations import Locations
-from .api.vendor_bills import VendorBills
-from .api.vendors import Vendors
-from .api.subsidiaries import Subsidiaries
-from .api.journal_entries import JournalEntries
-from .api.employees import Employees
-from .api.expense_reports import ExpenseReports
-from .api.folders import Folders
-from .api.files import Files
-from .api.customers import Customers
-from .api.projects import Projects
-from .api.expense_categories import ExpenseCategory
 from .api.custom_lists import CustomLists
 from .api.custom_records import CustomRecords
+from .api.customers import Customers
+from .api.departments import Departments
+from .api.employees import Employees
+from .api.expense_categories import ExpenseCategory
+from .api.expense_reports import ExpenseReports
+from .api.files import Files
+from .api.folders import Folders
+from .api.inventory_item import InventoryItem
+from .api.journal_entries import JournalEntries
+from .api.locations import Locations
+from .api.projects import Projects
+from .api.sales_orders import SalesOrder
+from .api.subsidiaries import Subsidiaries
+from .api.vendors import Vendors
+from .api.vendor_bills import VendorBills
 from .api.vendor_payments import VendorPayments
 from .internal.client import NetSuiteClient
 
@@ -32,20 +34,22 @@ class NetSuiteConnection:
         self.client = ns_client
         self.accounts = Accounts(ns_client)
         self.classifications = Classifications(ns_client)
-        self.departments = Departments(ns_client)
         self.currencies = Currencies(ns_client)
-        self.locations = Locations(ns_client)
-        self.vendor_bills = VendorBills(ns_client)
-        self.vendors = Vendors(ns_client)
-        self.subsidiaries = Subsidiaries(ns_client)
-        self.journal_entries = JournalEntries(ns_client)
-        self.employees = Employees(ns_client)
-        self.expense_reports = ExpenseReports(ns_client)
-        self.folders = Folders(ns_client)
-        self.files = Files(ns_client)
-        self.expense_categories = ExpenseCategory(ns_client)
         self.custom_lists = CustomLists(ns_client)
         self.custom_records = CustomRecords(ns_client)
         self.customers = Customers(ns_client)
+        self.departments = Departments(ns_client)
+        self.employees = Employees(ns_client)
+        self.expense_categories = ExpenseCategory(ns_client)
+        self.expense_reports = ExpenseReports(ns_client)
+        self.folders = Folders(ns_client)
+        self.inventory_items = InventoryItem(ns_client)
+        self.journal_entries = JournalEntries(ns_client)
+        self.locations = Locations(ns_client)
+        self.files = Files(ns_client)
         self.projects = Projects(ns_client)
+        self.sales_orders = SalesOrder(ns_client)
+        self.subsidiaries = Subsidiaries(ns_client)
+        self.vendors = Vendors(ns_client)
         self.vendor_payments = VendorPayments(ns_client)
+        self.vendor_bills = VendorBills(ns_client)
